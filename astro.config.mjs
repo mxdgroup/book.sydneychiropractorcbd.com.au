8ecac60e8c1c90d89905c9d536170c383f2dcfbb
dxmv <dimitirjestepanovic1@gmail.com>
2025-03-20 15:37:29 +0100

    Added compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+    domains: [],
+    remotePatterns: [],
+  },
+  integrations: [
+    compress({
+      CSS: true,
+      HTML: true,
+      JavaScript: true,
+      Image: false,
+      SVG: true
+    })
+  ]
+});
